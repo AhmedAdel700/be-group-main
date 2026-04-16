0f42f03732a948e5d50b4d0d473bd3222c5e138b
Ahmed Adel <AhmedAdel.Engineer1@gmail.com>
2026-04-16 14:09:16 +0200

    fixed the build bug

--- app/[locale]/sectors/[id]/page.tsx
+++ app/[locale]/sectors/[id]/page.tsx
@@ -1,6 +1,4 @@
-import Partners from "@/components/partners/Partners";
 import SectorDetails from "./SectorDetails";
-import Reviews from "@/components/reviews/Reviews";
 
 export default function Page() {
   return <SectorDetails />
